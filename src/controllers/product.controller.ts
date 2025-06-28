93b72d44bd476b4a12e90f4c0b2a34b965f74711
Henry Castillo <hdaniel031499@gmail.com>
2025-06-28 10:44:24 -0500

    .

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -17,7 +17,20 @@ const createProduct = async (req: Request, res: Response) => {
     return res.status(500).json({ message: error.message });
   }
 };
+const updatedProduct = async (req: Request, res: Response) => {
+  if (!req.body.pid) {
+    throw new Error("No se encontro ninguna id");
+  }
+
+  try {
+    const productUpdated = await ProductServices.updatedProduct(req.body);
+    return res.status(201).json(productUpdated);
+  } catch (error: any) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 export default {
   getProduct,
   createProduct,
+  updatedProduct,
 };
